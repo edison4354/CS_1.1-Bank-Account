6864b3463eecf50dbcb3c8c6b10a5890b6e781c0
Edison Li <edison4354@gmail.com>
2020-11-08 19:18:26 -0800

    Add Comments

--- BankAccount.py
+++ BankAccount.py
@@ -7,11 +7,30 @@ class BankAccount:
     self.routing_number = 98765432
     self.balance = float(0)
   
-  def deposit(self, amount):
+  # def deposit(self, amount):
+  #   self.balance += float(amount)
+  #   return f"Amount Deposited: ${amount}"
+  
+  # def withdraw(self, amount):
+  #   self.balance -= float(amount)
+  #   if self.balance >= 0:
+  #     return f"Amount Withdraw: ${amount}"
+  #   else: 
+  #     self.balance -= 10 
+  #     return f"Insufficient funds. An overdraft fee of $10 has been charged."
+
+  def deposit(self):
+    """I've used a input function for "amount" instead of a parameter so the user can enter the amount they want to deposit in the termal instead of in the code"""
+
+    amount = input("Please enter how much you would like to deposit:")
     self.balance += float(amount)
     return f"Amount Deposited: ${amount}"
   
-  def withdraw(self, amount):
+
+  def withdraw(self):
+    """I've used a input function for "amount" instead of a parameter so the user can enter the amount they want to deposit in the termal instead of in the code"""
+
+    amount = input("Please enter how much you would like to withdraw:")
     self.balance -= float(amount)
     if self.balance >= 0:
       return f"Amount Withdraw: ${amount}"
@@ -19,22 +38,27 @@ class BankAccount:
       self.balance -= 10 
       return f"Insufficient funds. An overdraft fee of $10 has been charged."
   
+
   def get_balance(self):
     return f"Thank for using Chase Bank, your current account balance is ${self.balance}"
+
 
   def add_interest(self):
     interest = self.balance * 0.00083
     self.balance += interest
     return f"Your interest got added for this month!"
 
+
   def print_receipt(self):
     return f"{self.full_name}\nAccount No.: {self.account_number}\nRouting No.: {self.routing_number}\nBalance: ${self.balance}"
 
+# Creating the Objects
 edison_account = BankAccount("Edison Li")
 nolan_account = BankAccount("Nolan Ngo")
 sergio_account = BankAccount("Sergio Cuh")
 iggi_account = BankAccount("Iggi jim")
 
-print(edison_account.deposit(90))
-print(nolan_account.withdraw(92))
-print(nolan_account.print_receipt())
+# Object Methods
+print(edison_account.deposit())
+print(edison_account.withdraw())
+print(edison_account.print_receipt())
