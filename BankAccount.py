6728b5bfbfddad05d5ef180f0f85f65a42dd9553
Edison Li <edison4354@gmail.com>
2020-11-08 19:45:51 -0800

    Added more commits for the methods

--- BankAccount.py
+++ BankAccount.py
@@ -19,17 +19,17 @@ class BankAccount:
   #     self.balance -= 10 
   #     return f"Insufficient funds. An overdraft fee of $10 has been charged."
 
+  # Method that returns a input for the amount you wish to deposit 
   def deposit(self):
     """I've used a input function for "amount" instead of a parameter so the user can enter the amount they want to deposit in the termal instead of in the code"""
-
     amount = input("Please enter how much you would like to deposit:")
     self.balance += float(amount)
     return f"Amount Deposited: ${amount}"
-  
 
+
+  # Method that returns a input for the amount you wish to withdraw 
   def withdraw(self):
     """I've used a input function for "amount" instead of a parameter so the user can enter the amount they want to deposit in the termal instead of in the code"""
-
     amount = input("Please enter how much you would like to withdraw:")
     self.balance -= float(amount)
     if self.balance >= 0:
@@ -37,18 +37,21 @@ class BankAccount:
     else: 
       self.balance -= 10 
       return f"Insufficient funds. An overdraft fee of $10 has been charged."
-  
 
+
+  # Method that returns the current balance of your account
   def get_balance(self):
     return f"Thank for using Chase Bank, your current account balance is ${self.balance}"
 
 
+  # Method that returns a new balance with the interest added
   def add_interest(self):
     interest = self.balance * 0.00083
     self.balance += interest
     return f"Your interest got added for this month!"
 
 
+  # Method that returns a receipt of your name, account number, routing number, and balance
   def print_receipt(self):
     return f"{self.full_name}\nAccount No.: {self.account_number}\nRouting No.: {self.routing_number}\nBalance: ${self.balance}"
 
@@ -60,5 +63,8 @@ iggi_account = BankAccount("Iggi jim")
 
 # Object Methods
 print(edison_account.deposit())
-print(edison_account.withdraw())
+print(edison_account.add_interest())
 print(edison_account.print_receipt())
+
+print(nolan_account.withdraw())
+print(nolan_account.print_receipt())
